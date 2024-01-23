9a3dd302b9139ee5b5beb3cf6c7ff0a120946ff2
zavianmali23.com <z.avianmali2020@gmail.com>
2024-01-23 05:49:11 +0300

    Interface

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue';
+import Job from './types/Job'
 
-const state = reactive({
-  name: 'Jackson',
-  age: 24
-})
-
-// state.name = 9;
-
-let first_name = ref<string>()
-let age = ref<number>()
-
-
-first_name.value = 'joe'
-age.value = 39
+const jobs = ref<Job[]>([
+  { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+  { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+  { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+  { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+  { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+])
 </script>
 
 <template>
   <div>
     <div class="app">
-      hello, {{ state.name }} <br>
-        Welcom, {{ first_name }} <br>
-          age, {{ age }}
+      <div class="" v-for="job in jobs" :key="job.id">
+        {{ job.title }}
+      </div>
     </div>
   </div>
 </template>
