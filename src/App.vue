15034c1a1ae5ea34017ade8a5bde698d6760739b
zavianmali23.com <z.avianmali2020@gmail.com>
2024-01-23 05:09:03 +0300

    Vue Typescript

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const state = reactive({
+  name: 'Jackson',
+  age: 24
+})
+
+// state.name = 9;
+
+let first_name = ref<string>()
+let age = ref<number>()
+
+
+first_name.value = 'joe'
+age.value = 39
+</script>
+
+<template>
+  <div>
+    <div class="app">
+      hello, {{ state.name }} <br>
+        Welcom, {{ first_name }} <br>
+          age, {{ age }}
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
